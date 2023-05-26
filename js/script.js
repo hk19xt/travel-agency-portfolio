1fcdbf266a48fcdb00cbdfb4bb599203b9449b2d
Hyejin Kim <hk19xt@brocku.ca>
2023-05-26 17:03:03 -0400

    slider completed

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
-$(".owl-carousel").owlCarousel({
-  loop: true,
-  margin: 0,
-  nav: false,
-  autoplay: true,
-  autoplayTimeout: 6000,
-  autoplayHoverPause: false,
-  responsive: {
-    0: {
-      items: 1,
+jQuery(document).ready(function ($) {
+  $("#owl-demo").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 6000,
+    autoplayHoverPause: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
     },
-    600: {
-      items: 1,
-    },
-    1000: {
-      items: 1,
-    },
-  },
+  });
 });
